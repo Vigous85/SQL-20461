882cbf88130485ee2e85fbf4e8df94402ec5a97c
Vigous85 <96003049+Vigous85@users.noreply.github.com>
2022-02-23 16:35:16 +0400

    Add files via upload

---
+++ Lab07/Starter/Project/Project/41 - Lab Exercise 1.sql
@@ -0,0 +1,18 @@
+USE TSQL
+
+/***************************************************************
+Task 1
+
+Write an INSERT statement to add a record to the Employees table 
+with the following values:
+•Title: Sales Representative
+•Titleofcourtesy: Mr
+•FirstName: Laurence
+•Lastname: Grider
+•Hiredate: 04/04/2013
+•Birthdate: 10/25/1975
+•Address: 1234 1st Ave. S.E.
+•City: Seattle
+•Country: USA
+•Phone: (206)555-0105
+*****************************************************************/
